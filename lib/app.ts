ed2426307282dcba9335a31d49c397347bcc0f53
Flavio Espinoza <flavio.espinoza@gmail.com>
2019-02-21 17:38:57 -0700

    formatted

--- lib/app.ts
+++ lib/app.ts
@@ -1,6 +1,6 @@
 import * as express from 'express'
 import bodyParser = require('body-parser')
-import {Request, Response} from 'express'
+import { Request, Response } from 'express'
 
 const log = require('ololog').configure({
     locate: false
@@ -15,13 +15,15 @@ const elasticsearch = require('elasticsearch')
 const es = new elasticsearch.Client({
     // host: 'localhost:9200',
     // host: 'http://178.128.190.197:9200',
-    hosts: [{
-        protocol: 'http',
-        host: '178.128.190.197',
-        port: 9200,
-        country: 'US',
-        weight: 10
-    }],
+    hosts: [
+        {
+            protocol: 'http',
+            host: '178.128.190.197',
+            port: 9200,
+            country: 'US',
+            weight: 10
+        }
+    ],
     log: 'trace'
 })
 
@@ -29,7 +31,6 @@ let crypto_arr: Array<object> = []
 let user_agent = null
 
 class App {
-
     constructor() {
         this.app = express()
         this.config()
@@ -40,32 +41,29 @@ class App {
 
     private config(): void {
         this.app.use(bodyParser.json())
-        this.app.use(bodyParser.urlencoded({extended: false}))
-        this.app.use(function (req, res, next) {
+        this.app.use(bodyParser.urlencoded({ extended: false }))
+        this.app.use(function(req, res, next) {
             user_agent = req.get('User-Agent')
             next()
         })
     }
 
     private routes(): void {
-
         const self = this
         const router = express.Router()
 
-        router.get('/', function (req: Request, res: Response) {
-
-            self.get_data('/').then( (response) => {
-
+        router.get('/', function(req: Request, res: Response) {
+            self.get_data('/').then(response => {
                 // log.red('crypto_arr', JSON.stringify(crypto_arr, null, 2))
 
                 let candel_obj_model = {
-                    "time": 1539548160,
-                    "close": 6398.75,
-                    "high": 6399.07,
-                    "low": 6395,
-                    "open": 6398.17,
-                    "volumefrom": 2.94,
-                    "volumeto": 18810.2
+                    time: 1539548160,
+                    close: 6398.75,
+                    high: 6399.07,
+                    low: 6395,
+                    open: 6398.17,
+                    volumefrom: 2.94,
+                    volumeto: 18810.2
                 }
 
                 let exchange_name = 'hitbtc'
@@ -73,10 +71,8 @@ class App {
 
                 log.blue('crypto_arr', crypto_arr)
 
-                _.each(crypto_arr, function (candle_obj) {
-
-                    (async function (){
-
+                _.each(crypto_arr, function(candle_obj) {
+                    ;(async function() {
                         let _id = `${exchange_name}__${market_name}___${candle_obj.timestamp}`
 
                         let exists = await es.exists({
@@ -91,35 +87,26 @@ class App {
                                 type: 'BTC_USD',
                                 id: _id,
                                 body: candle_obj
-                            });
+                            })
                         }
-
                     })()
-
                 })
 
                 res.status(200).send(crypto_arr)
-
             })
-
         })
 
-
-        router.post('/', function (req: Request, res: Response) {
-
+        router.post('/', function(req: Request, res: Response) {
             log.lightYellow('post', '/')
 
             const data = req.body
             res.status(200).send(data)
-
         })
 
         this.app.use('/', router)
-
     }
 
     private rest_client(market_name: string, url: string, market_info: object): Promise<string> {
-
         const args = {
             headers: {
                 'User-Agent': user_agent
@@ -131,12 +118,10 @@ class App {
         let _exchange_name = 'hitbtc'
 
         return new Promise<string>((resolve, reject) => {
-
             client.get(url, args, (res_data, res) => {
-
                 let fuck = res_data.Data
 
-                _.each(res_data.Data, function (candle_obj) {
+                _.each(res_data.Data, function(candle_obj) {
                     let timestamp = candle_obj.time * 1000
                     let date = new Date(timestamp)
                     candle_obj.timestamp = timestamp
@@ -146,13 +131,10 @@ class App {
 
                 resolve(res_data)
             })
-
         })
-
     }
 
     private async get_data(route: string): Promise<string> {
-        
         const self = this
 
         let _interval = '1m'
@@ -205,27 +187,27 @@ class App {
                 base: 'BTC',
                 quote: 'USDT',
                 symbol: 'BTC/USDT'
-            },
+            }
         ]
 
-        let _market_name = function (sym) {
+        let _market_name = function(sym) {
             return sym.replace('/', '_')
         }
 
-        let _url = function (base, quote) {
-
+        let _url = function(base, quote) {
             return `https://min-api.cryptocompare.com/data/histominute?fsym=${base}&tsym=${quote}&aggregate=1&e=hitbtc`
         }
 
-        await this.rest_client(_market_name(_test_markets[0].symbol), _url(_test_markets[0].base, _test_markets[0].quote), _test_markets[0])
-
+        await this.rest_client(
+            _market_name(_test_markets[0].symbol),
+            _url(_test_markets[0].base, _test_markets[0].quote),
+            _test_markets[0]
+        )
 
         return new Promise<string>((resolve, reject) => {
             resolve(route)
         })
-
     }
-
 }
 
 export default new App().app
